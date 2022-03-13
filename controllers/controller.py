4b4bc5941792835fb8f148a5f04a777189ee62af
Annika Neumann <annika1983neumann@gmail.com>
2022-03-13 21:28:56 +0000

    added css, color, images, layout

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,28 +9,30 @@ from modules.player import *
 # def index():
 #     return render_template('index.html')
 
+# @app.route('/game_start')
+# def make_choices():
+#     return render_template('game_start.html')
+
 @app.route('/<player_one>/<player_two>')
+# the above variables refer to the choice of each player which we use in the url
 # the above route takes in two variables, these two variables are being passed into the function, 
 # the Function below MUST accept these variables, else it will error out, so two variables from browser means you need two into the function
 def start_of_game(player_one,player_two):   
 
     # these variables below are used to play the game and comes directly from the browser input
-    player1 = player_one
-    player2 = player_two
+    # below are two instances of the class Player, which contains the player's name and their choice
+    player1 = Player("Jamie", player_one)
+    player2 = Player("Claire", player_two)
     # the below variable game is a function from the class game, which is then used to run the actual game, passing in the two player choices
     # game = Game(player1,player2)
     result = Game.let_the_games_begin(player1,player2)
     
     # the variables passed into the render_template becomes available on the webpage refered to in the single quotes, 
-    return render_template('index.html',player1Choice = player1, player2Choice = player2, result = result)
+    return render_template('index.html',player1web = player1, player2web = player2, result = result)
 
 
-# @app.route('/result')
-# def let_the_games_begin():
-
-
-#     return render_template('rock_paper_scissors.html', rock_paper_scissors = "Testing stuff", title='Home')
-
-
-# @app.route('/choice_p1, choice_p2')
-# def let_the_games_begin(p1, p2):
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+     
+    
--- modules/game.py
+++ modules/game.py
@@ -3,27 +3,27 @@ class Game:
     # when creating a class, you do not always need an init function if you are not storing variables for later, 
     # you are able to just have helper functions do the logic you need, making sure to pass all the data/variables you need into the function
 
-    def let_the_games_begin(p1_choice,p2_choice):
-        if p1_choice == p2_choice:
+    def let_the_games_begin(p1,p2):
+        if p1.choice == p2.choice:
             return "It's a draw"
 
-        elif p1_choice == "paper" and p2_choice == "rock":
-            return "Player 1 wins"
+        elif p1.choice == "paper" and p2.choice == "rock":
+            return f"{p1.name} won"
 
-        elif p1_choice == "paper" and p2_choice == "scissors":
-            return "Player 2 win"
+        elif p1.choice == "paper" and p2.choice == "scissors":
+            return f"{p2.name} won"
 
-        elif p1_choice == "rock" and p2_choice == "scissors":
-            return "Player 1 win"
+        elif p1.choice == "rock" and p2.choice == "scissors":
+            return f"{p1.name} won"
         
-        elif p1_choice == "rock" and p2_choice == "paper":
-            return "Player 2 win"
+        elif p1.choice == "rock" and p2.choice == "paper":
+            return f"{p2.name} won"
                 
-        elif p1_choice == "scissors" and p2_choice == "rock":
-            return "Player 2 wins"
+        elif p1.choice == "scissors" and p2.choice == "rock":
+            return f"{p2.name} won"
         
-        elif p1_choice == "scissors" and p2_choice == "paper":
-            return "Player 1 wins"
+        elif p1.choice == "scissors" and p2.choice == "paper":
+            return f"{p1.name} won"
        
      
 
